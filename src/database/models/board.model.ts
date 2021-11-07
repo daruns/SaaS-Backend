0ee9d51a257028742f3135f81556bae0d71c8dd5
daruns <darun.s.omar@gmail.com>
2021-11-07 21:03:40 +0300

    add relationship model from board to tasks

--- src/database/models/board.model.ts
+++ src/database/models/board.model.ts
@@ -2,6 +2,7 @@ import { BaseModel } from './base.model';
 import { Model } from 'objection';
 import { ProjectModel } from './project.model';
 import { BoardAttributeModel } from './boardAttribute.model';
+import { TaskModel } from './task.model';
 
 const tableName = 'projectBoards'
 export class BoardModel extends BaseModel {
@@ -14,8 +15,18 @@ export class BoardModel extends BaseModel {
 
   boardAttribute: BoardAttributeModel[];
   project: ProjectModel;
+  tasks: TaskModel[];
 
   static relationMappings = {
+    tasks: {
+    // list of all tasks on current user
+      modelClass: `${__dirname}/task.model`,
+      relation: Model.HasManyRelation,
+      join: {
+        from: `${tableName}.id`,
+        to: 'tasks.boardId',
+      },
+    },
     // list of all client on current user
     boardAttribute: {
       modelClass: `${__dirname}/boardAttribute.model`,
